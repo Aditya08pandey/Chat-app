c337f8b5fe7b6dd256422bdd2cfbd8df90a15314
Aditya Pandey <pandeydevil0802@gmail.com>
2025-01-21 19:08:12 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,12 @@ import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
-//const app=express();
+dotenv.config(); 
+
 
 const PORT = process.env.PORT||5000
-
 const __dirname = path.resolve();
 
-
-dotenv.config(); 
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser());
@@ -24,14 +22,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
 })
-
-
-
+}
 
 server.listen(PORT, ()=>{
     connectToMongoDB();
